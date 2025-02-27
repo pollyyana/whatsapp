76ae123e46e93c04cd39abc0bc0d6759e0a1cd7d
pollyyana <pollyana.medeirosk@gmail.com>
2025-02-27 01:44:26 -0300

    ajuste manter-usuario

--- lib/home.dart
+++ lib/home.dart
@@ -2,47 +2,34 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  User? user; // Variável para armazenar o usuário logado
-
-  @override
-  void initState() {
-    super.initState();
-    verificarUsuarioLogado();
-  }
-
-  void verificarUsuarioLogado() {
-    user = FirebaseAuth.instance.currentUser; // Obtém o usuário logado
-    if (user == null) {
-      // Se não houver usuário logado, redireciona para a tela de login
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const Login()),
-        );
-      });
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
         backgroundColor: const Color(0xFF6C63FF),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Login(initialEmail: user?.email ?? ""),
+              ),
+            );
+          },
+        ),
       ),
       body: Center(
         child: user != null
-            ? Text(' ${user!.email}',
+            ? Text('Bem-vindo, ${user.email}',
                 style: const TextStyle(fontSize: 18, color: Colors.black))
-            : const CircularProgressIndicator(), // Caso o usuário não esteja carregado ainda
+            : const CircularProgressIndicator(),
       ),
     );
   }
